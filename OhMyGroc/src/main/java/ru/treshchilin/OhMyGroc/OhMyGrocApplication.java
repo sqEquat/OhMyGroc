eddc5533728a0f97ec7b25bcbc518565aaf489b7
sqEquat <nikitatresh@gmail.com>
2022-03-03 18:02:37 +0300

    Init commit

---
+++ OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/OhMyGrocApplication.java
@@ -0,0 +1,13 @@
+package ru.treshchilin.OhMyGroc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMyGrocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhMyGrocApplication.class, args);
+	}
+
+}
---
+++ OhMyGroc/src/test/java/ru/treshchilin/OhMyGroc/OhMyGrocApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.treshchilin.OhMyGroc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMyGrocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
