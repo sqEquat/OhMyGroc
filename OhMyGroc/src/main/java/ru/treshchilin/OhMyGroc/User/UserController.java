9c2ab8ca096ec49c14421ce0a429c8c809d033cd
sqEquat <nikitatresh@gmail.com>
2022-03-03 19:17:34 +0300

    Move user controller to the user package

--- OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/User/UserController.java
+++ OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/User/UserController.java
@@ -1,18 +1,19 @@
-package ru.treshchilin.OhMyGroc.Controllers;
+package ru.treshchilin.OhMyGroc.User;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ru.treshchilin.OhMyGroc.User.User;
-
 @RestController
-public class FirstRestController {
+@RequestMapping("api/v1/user")
+public class UserController {
 	
 	@GetMapping
 	public List<User> hello() {
 		return List.of(
 				new User(1L, "user@mail.com", "User", List.of("Milk", "Tomatoes", "Tea", "Bread")));
 	}
+
 }
