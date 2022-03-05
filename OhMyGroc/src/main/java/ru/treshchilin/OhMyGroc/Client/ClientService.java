e2d7c80de6083d1da7fcdc9bdf78c867a78ea249
sqEquat <nikitatresh@gmail.com>
2022-03-05 20:57:14 +0300

    Refactor client service to work with repo

--- OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/Client/ClientService.java
+++ OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/Client/ClientService.java
@@ -2,13 +2,20 @@ package ru.treshchilin.OhMyGroc.Client;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
 	
+	private final ClientRepository clientRepository;
+	
+	@Autowired
+	public ClientService(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
+
 	public List<Client> getUsers() {
-		return List.of(
-				new Client(1L, "equat@mail.com", "Equat", List.of("Milk", "Tomatoes", "Tea", "Bread", "Epica")));
+		return clientRepository.findAll();
 	}
 }
