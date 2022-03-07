d28e7f13ca9a9c24331cf495305f3442a0ed9bdf
sqEquat <nikitatresh@gmail.com>
2022-03-08 00:07:20 +0300

    Add test delete client if does not exists

--- OhMyGroc/src/test/java/ru/treshchilin/OhMyGroc/Client/ClientServiceTest.java
+++ OhMyGroc/src/test/java/ru/treshchilin/OhMyGroc/Client/ClientServiceTest.java
@@ -1,8 +1,12 @@
 package ru.treshchilin.OhMyGroc.Client;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -10,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
 
 @ExtendWith(MockitoExtension.class)
 class ClientServiceTest {
@@ -39,6 +42,18 @@ class ClientServiceTest {
 		
 		verify(clientRepository).save(client);
 	}
+	
+	@Test
+	void testAddNewClientIfEmailIsOccupied() {
+		Client client = new Client("test@test.com", "Test");
+		when(clientRepository.findByEmail(client.getEmail())).thenReturn(List.of(client));
+		
+		assertThrows(IllegalStateException.class, () -> {
+			underTest.addNewClient(client);
+		});
+		
+		verify(clientRepository, never()).save(client);
+	}
 
 	@Test
 	void testDeleteClientIfExists() {
@@ -48,6 +63,19 @@ class ClientServiceTest {
 		underTest.deleteClient(clientId);
 		
 		verify(clientRepository).deleteById(clientId);
+	}
+	
+	@Test
+	void testDeleteClientIfDoesNotExists() {
+		Long clientId = 1L;
+		when(clientRepository.existsById(clientId)).thenReturn(false);
+		
+		
+		assertThrows(IllegalStateException.class, () -> {
+			underTest.deleteClient(clientId);
+		});
+		
+		verify(clientRepository, never()).deleteById(clientId);
 	}
 
 	@Test
