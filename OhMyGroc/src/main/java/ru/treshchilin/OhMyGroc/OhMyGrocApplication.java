e8ed580cd91d2ad8804bb662a8e1c3205a4cde27
sqEquat <nikitatresh@gmail.com>
2022-03-17 00:34:25 +0300

    Delete command line runner with two clients

--- OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/OhMyGrocApplication.java
+++ OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/OhMyGrocApplication.java
@@ -1,30 +1,12 @@
 package ru.treshchilin.OhMyGroc;
 
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import ru.treshchilin.OhMyGroc.model.Client;
-import ru.treshchilin.OhMyGroc.repo.ClientRepository;
 
 @SpringBootApplication
 public class OhMyGrocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OhMyGrocApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(ClientRepository repository) {
-		return args -> {
-			Client nick = new Client("equat@mail.com", "Equat", List.of("Milk", "Tomatoes", "Tea", "Bread", "Epica"));
-
-			Client vova = new Client("vtresh@mail.com", "Vtresh", List.of("Milk", "Beer", "Bread", "Fish"));
-			repository.saveAll(List.of(nick, vova));
-		};
-
 	}
 }
