e29c59b94a8df8f6256e9e0fa5c3b41b3dddac39
sqEquat <nikitatresh@gmail.com>
2022-04-12 22:23:02 +0300

    Add BCryptPasswordEncoder bean

--- OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/OhMyGrocApplication.java
+++ OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/OhMyGrocApplication.java
@@ -2,11 +2,19 @@ package ru.treshchilin.OhMyGroc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class OhMyGrocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OhMyGrocApplication.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
