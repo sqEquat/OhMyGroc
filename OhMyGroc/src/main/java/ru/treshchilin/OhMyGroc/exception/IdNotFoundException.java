dd4677b8c98298e4d89cfbabbfb080c81cd9d2e7
sqEquat <nikitatresh@gmail.com>
2022-05-02 17:54:29 +0300

    Generated serial version UID

--- OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/exception/IdNotFoundException.java
+++ OhMyGroc/src/main/java/ru/treshchilin/OhMyGroc/exception/IdNotFoundException.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class IdNotFoundException extends RuntimeException{
 
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = -3626624899959123899L;
+
 	public IdNotFoundException(String message) {
 		super(message);
 	}
